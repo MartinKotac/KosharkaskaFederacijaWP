0c4cbf48ac859abd490533e5088b47ea510bae77
Aleksandar_Jankulovski <aleksandar.j516@gmail.com>
2022-03-16 21:25:55 +0100

    Creating the app

---
+++ src/main/java/ukim/finki/mk/kosharkaskafederacija/KosharkaskaFederacijaApplication.java
@@ -0,0 +1,13 @@
+package ukim.finki.mk.kosharkaskafederacija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosharkaskaFederacijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KosharkaskaFederacijaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ukim/finki/mk/kosharkaskafederacija/KosharkaskaFederacijaApplicationTests.java
@@ -0,0 +1,13 @@
+package ukim.finki.mk.kosharkaskafederacija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosharkaskaFederacijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
